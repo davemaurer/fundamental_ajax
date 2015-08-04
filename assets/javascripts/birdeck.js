c0d69343d54159f378c1ee8f2e034390b5704d99
David Maurer <davemaurermail@gmail.com>
2015-08-04 10:50:29 -0600

    added delete button to line 19,44,67 of birdeck.js so we can delete posts

--- assets/javascripts/birdeck.js
+++ assets/javascripts/birdeck.js
@@ -15,6 +15,8 @@ $(document).ready(function(){
           + "<p>"
           + post.description
           + "</p>"
+            //to create a button for deleting
+          + "<button id='delete-post' name='delete-button' class='btn btn-default btn-xs'>Delete</button>"
           + "</div>"
         ) //this appends the posts from .each to our div in index.html called latest-posts
       })
@@ -39,8 +41,33 @@ $(document).ready(function(){
           + "<p>"
           + newPost.description
           + "</p>"
+          + "<button id='delete-post' name='delete-button' class='btn btn-default btn-xs'>Delete</button>"
           + "</div>"
           )
+      }
+    })
+  })
+  $('#fetch-posts').on('click', function(){
+    $.ajax({
+      type: 'GET', //GET is the type of request we are sending
+      url: 'http://turing-birdie.herokuapp.com/api/v1/posts.json', //This is the url we are sending it to
+      //success means if this is successful do something
+      success: function(posts) {
+        //$.each(posts, function needs two arguments, 1 - index, 2, thing you want to iterate
+        $.each(posts, function(index, post) {
+          $('#latest-posts').append(
+            "<div class='post' data-id='"
+            + post.id
+            + "'><h6>Published on "
+            + post.created_at
+            + "</h6>"
+            + "<p>"
+            + post.description
+            + "</p>"
+            + "<button id='delete-post' name='delete-button' class='btn btn-default btn-xs'>Delete</button>"
+            + "</div>"
+          ) //this appends the posts from .each to our div in index.html called latest-posts
+        })
       }
     })
   })
